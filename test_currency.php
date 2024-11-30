f0ff0908ea3bb9f108963fd70aa9fbb131629ed1
unknown <mahshidkhodsiani@yahoo.com>
2024-11-30 17:48:27 +0330

    030-ads

--- test_currency.php
+++ test_currency.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection settings
-$host = 'localhost'; // Change as needed
-$username = 'root'; // Your database username
-$password = ''; // Your database password
-$dbname = 'adsbarg'; // Replace with your database name
+$host = '185.141.212.171'; // Change as needed
+$username = 'adsbarg_admin'; // Your database username
+$password = 'HL(to{PCYL=b'; // Your database password
+$dbname = 'adsbarg_dashboard'; // Replace with your database name
 
 // Create a database connection
 $conn = new mysqli($host, $username, $password, $dbname);
